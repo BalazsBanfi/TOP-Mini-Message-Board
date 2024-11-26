7678fa32778c566894cad34709ce07acc504d097
BalazsBanfi <banfibalazs81@gmail.com>
2024-11-26 09:59:17 +0100

    Add new message

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 const messages = [
@@ -19,16 +20,37 @@ const messages = [
 
 app.get('/', (req, res) => {
     res.render('pages/index', {
-        messages,
-        title: "Home Page"
+        messages: messages,
+        title: "Mini messageboard"
     })
 })
 app.get('/new', (req, res) => {
-    res.render('pages/new', {
-        messages,
+    res.render('pages/form', {
+        messages: messages,
         title: "New message"
     })
 })
+
+app.get('/:id', (req, res) => {
+    if (messages[req.params.id] !== undefined) {
+        res.render('pages/message', {
+            messages: messages,
+            title: "One message",
+            messageId: req.params.id
+        })
+    } else {
+        res.render('pages/404')
+    }
+}
+)
+
+app.post('/new', (req, res) => {
+    let userMessage = req.body.userMessage;
+    let userName = req.body.userName;
+    messages.unshift({ text: userMessage, user: userName, added: new Date() });
+    res.redirect('/')
+})
+
 
 app.listen(port, () => {
     console.log(`App listening at port ${port}`)
