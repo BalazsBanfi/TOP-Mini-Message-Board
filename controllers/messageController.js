2542e676fd6983d721f1613dfd4ba921f810a759
BalazsBanfi <banfibalazs81@gmail.com>
2024-12-21 13:15:16 +0100

    Add validation method

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,6 +1,22 @@
 const db = require('../db/queries');
+const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 const { CustomDbError } = require('../errors/CustomErrors');
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+const lengthMessageErr = "must be maximum 200 characters.";
+
+const validateUser = [
+    body("userName").trim()
+        .notEmpty().withMessage("Username can not be empty.")
+        .isAlpha().withMessage(`Username ${alphaErr}`)
+        .isLength({ min: 1, max: 10 }).withMessage(`Username ${lengthErr}`),
+    body("userMessage").trim()
+        .notEmpty().withMessage("Message can not be empty.")
+        .isLength({ max: 200 }).withMessage(`Message ${lengthMessageErr}`),
+];
+
 
 const getMessages = asyncHandler(async (req, res) => {
     const messages = await db.getAllMessages();
@@ -43,11 +59,29 @@ const getMessageById = asyncHandler(async (req, res) => {
         title: "Selected message",
     });
 })
-
+/*
 const postNewMessage = asyncHandler(async (req, res) => {
     await db.insertMessage(req.body.userName, req.body.userMessage, new Date());
     res.redirect("/");
 })
+*/
+
+const postNewMessage = [
+    validateUser,
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.render("pages/new", {
+                title: "New message",
+                errors: errors.array(),
+            });
+        }
+        const { userName, userMessage } = req.body;
+        db.insertMessage(userName, userMessage, new Date());
+        res.redirect("/");
+    }
+];
+
 
 const getDeleteMessages = asyncHandler(async (req, res) => {
     await db.deleteMessages();
